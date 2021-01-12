295ea45896eecceda80d9c5c93078dbc807bd19e
Ruslan Badaev <rusik.frost@gmail.com>
2021-01-13 00:25:44 +0300

    change clients port and build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f031ff7d8710a6b726403e26dd6e4bcd.js"
+  "/precache-manifest.7f6dce78c63a77da69fc6028f8c75a11.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "skf"});
