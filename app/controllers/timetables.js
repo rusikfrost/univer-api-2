0aedf79fd58360e8cbcf73131e044560558262c9
ruslanbadaev <r.badaeff@gmail.com>
2021-04-14 01:05:07 +0300

    add timetable request

--- app/controllers/timetables.js
+++ app/controllers/timetables.js
@@ -1,8 +1,10 @@
 const model = require('../models/timetable')
+const usersModel = require('../models/user')
+const groupsModel = require('../models/group')
 const { matchedData } = require('express-validator')
 const utils = require('../middleware/utils')
 const db = require('../middleware/db')
-const dbfstream = require('dbfstreammemo');
+const dbfstream = require('dbfstreammemo')
 /*********************
  * Private functions *
  *********************/
@@ -12,58 +14,62 @@ const dbfstream = require('dbfstreammemo');
  * @param {string} id - id of item
  * @param {string} name - name of item
  */
-const timetableExistsExcludingItself = async(id, name) => {
-    return new Promise((resolve, reject) => {
-        model.findOne({
-                name,
-                _id: {
-                    $ne: id
-                }
-            },
-            (err, item) => {
-                utils.itemAlreadyExists(err, item, reject, 'TIMETABLE_ALREADY_EXISTS')
-                resolve(false)
-            }
-        )
-    })
+const timetableExistsExcludingItself = async (id, name) => {
+  return new Promise((resolve, reject) => {
+    model.findOne(
+      {
+        name,
+        _id: {
+          $ne: id
+        }
+      },
+      (err, item) => {
+        utils.itemAlreadyExists(err, item, reject, 'TIMETABLE_ALREADY_EXISTS')
+        resolve(false)
+      }
+    )
+  })
 }
 
 /**
  * Checks if a timetable already exists in database
  * @param {string} name - name of item
  */
-const timetableExists = async(name) => {
-    return new Promise((resolve, reject) => {
-        model.findOne({
-                name
-            },
-            (err, item) => {
-                utils.itemAlreadyExists(err, item, reject, 'TIMETABLE_ALREADY_EXISTS')
-                resolve(false)
-            }
-        )
-    })
+const timetableExists = async (name) => {
+  return new Promise((resolve, reject) => {
+    model.findOne(
+      {
+        name
+      },
+      (err, item) => {
+        utils.itemAlreadyExists(err, item, reject, 'TIMETABLE_ALREADY_EXISTS')
+        resolve(false)
+      }
+    )
+  })
 }
 
 /**
  * Gets all items from database
  */
-const getAllItemsFromDB = async() => {
-    return new Promise((resolve, reject) => {
-        model.find({},
-            '-updatedAt -createdAt', {
-                sort: {
-                    name: 1
-                }
-            },
-            (err, items) => {
-                if (err) {
-                    reject(utils.buildErrObject(422, err.message))
-                }
-                resolve(items)
-            }
-        )
-    })
+const getAllItemsFromDB = async () => {
+  return new Promise((resolve, reject) => {
+    model.find(
+      {},
+      '-updatedAt -createdAt',
+      {
+        sort: {
+          name: 1
+        }
+      },
+      (err, items) => {
+        if (err) {
+          reject(utils.buildErrObject(422, err.message))
+        }
+        resolve(items)
+      }
+    )
+  })
 }
 
 /********************
@@ -75,13 +81,13 @@ const getAllItemsFromDB = async() => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.getAllItems = async(req, res) => {
-    try {
-        const result = await getAllItemsFromDB()
-        res.status(200).json({ errors: null, result })
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+exports.getAllItems = async (req, res) => {
+  try {
+    const result = await getAllItemsFromDB()
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -89,23 +95,35 @@ exports.getAllItems = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.getItems = async(req, res) => {
-    try {
-        const query = await db.checkQueryString(req.query)
-        const result = await db.getItems(req, model, query)
-        res.status(200).json({ errors: null, result })
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+exports.getItems = async (req, res) => {
+  try {
+    const query = await db.checkQueryString(req.query)
+    const result = await db.getItems(req, model, query)
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
-exports.getLastUpload = async(req, res) => {
-    try {
 
-        const result = await db.getItemByParams({ LOAD_ID: "1" }, model)
-        res.status(200).json({ errors: null, result })
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+exports.getLastUpload = async (req, res) => {
+  try {
+    const result = await db.getItemByParams({ LOAD_ID: '1' }, model)
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
+}
+
+exports.getItemByUser = async (req, res) => {
+  try {
+    console.log(req.params)
+    const user = await db.getItem(req.params.id, usersModel)
+    const group = await db.getItem(user.group_id, groupsModel)
+    const result = await db.getItemByParams({ GROUP: group.name }, model)
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -113,15 +131,15 @@ exports.getLastUpload = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.getItem = async(req, res) => {
-    try {
-        req = matchedData(req)
-        const id = await utils.isIDGood(req.id)
-        const result = await db.getItem(id, model)
-        res.status(200).json({ errors: null, result })
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+exports.getItem = async (req, res) => {
+  try {
+    req = matchedData(req)
+    const id = await utils.isIDGood(req.id)
+    const result = await db.getItem(id, model)
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -129,21 +147,21 @@ exports.getItem = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.updateItem = async(req, res) => {
-    try {
-        req = matchedData(req)
-        const id = await utils.isIDGood(req.id)
-        const doesTimetableExists = await timetableExistsExcludingItself(
-            id,
-            req.name
-        )
-        if (!doesTimetableExists) {
-            const result = await db.updateItem(id, model, req)
-            res.status(200).json({ errors: null, result })
-        }
-    } catch (error) {
-        utils.handleError(res, error)
+exports.updateItem = async (req, res) => {
+  try {
+    req = matchedData(req)
+    const id = await utils.isIDGood(req.id)
+    const doesTimetableExists = await timetableExistsExcludingItself(
+      id,
+      req.name
+    )
+    if (!doesTimetableExists) {
+      const result = await db.updateItem(id, model, req)
+      res.status(200).json({ errors: null, result })
     }
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -151,17 +169,17 @@ exports.updateItem = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.createItem = async(req, res) => {
-    try {
-        req = matchedData(req)
-        const doesTimetableExists = await timetableExists(req.name)
-        if (!doesTimetableExists) {
-            const result = await db.createItem(req, model)
-            res.status(201).json({ errors: null, result })
-        }
-    } catch (error) {
-        utils.handleError(res, error)
+exports.createItem = async (req, res) => {
+  try {
+    req = matchedData(req)
+    const doesTimetableExists = await timetableExists(req.name)
+    if (!doesTimetableExists) {
+      const result = await db.createItem(req, model)
+      res.status(201).json({ errors: null, result })
     }
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -169,33 +187,31 @@ exports.createItem = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.createItemFromDBF = async(req, res) => {
-    try {
-        db.clearCollection(model)
-        console.log(req.files)
-        const dbf = dbfstream(`./${req.files[0].path}`, 'cp866');
-        let a = []
-        dbf.on('header', header => {}).on('data', (data) => {});
-        dbf.on('data', (data) => {
-            console.log(data);
-            data.LOAD_ID = data['@numOfRecord']
-            db.createItem(data, model).catch(err => console.log(err))
-        });
-        dbf.on('error', (err) => {
-            console.log(err);
-        });
-        dbf.on('end', () => {
-            console.log('stream end');
-            //console.log(dbf);
-        });
+exports.createItemFromDBF = async (req, res) => {
+  try {
+    db.clearCollection(model)
+    console.log(req.files)
+    const dbf = dbfstream(`./${req.files[0].path}`, 'cp866')
+    let a = []
+    dbf.on('header', (header) => {}).on('data', (data) => {})
+    dbf.on('data', (data) => {
+      console.log(data)
+      data.LOAD_ID = data['@numOfRecord']
+      db.createItem(data, model).catch((err) => console.log(err))
+    })
+    dbf.on('error', (err) => {
+      console.log(err)
+    })
+    dbf.on('end', () => {
+      console.log('stream end')
+      //console.log(dbf);
+    })
 
-
-        //const result = await db.createItem(req, model)
-        res.status(201).json({ errors: null, result: 'ok' })
-
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+    //const result = await db.createItem(req, model)
+    res.status(201).json({ errors: null, result: 'ok' })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 /**
@@ -203,15 +219,15 @@ exports.createItemFromDBF = async(req, res) => {
  * @param {Object} req - request object
  * @param {Object} res - response object
  */
-exports.deleteItem = async(req, res) => {
-    try {
-        req = matchedData(req)
-        const id = await utils.isIDGood(req.id)
-        const result = await db.deleteItem(id, model)
-        res.status(200).json({ errors: null, result })
-    } catch (error) {
-        utils.handleError(res, error)
-    }
+exports.deleteItem = async (req, res) => {
+  try {
+    req = matchedData(req)
+    const id = await utils.isIDGood(req.id)
+    const result = await db.deleteItem(id, model)
+    res.status(200).json({ errors: null, result })
+  } catch (error) {
+    utils.handleError(res, error)
+  }
 }
 
 //console.log(data);
--- app/routes/timetable.js
+++ app/routes/timetable.js
@@ -6,7 +6,7 @@ const router = express.Router()
 require('../../config/passport')
 const passport = require('passport')
 const requireAuth = passport.authenticate('jwt', {
-    session: false
+  session: false
 })
 const trimRequest = require('trim-request')
 
@@ -14,95 +14,104 @@ const trimRequest = require('trim-request')
  * news routes
  */
 
+router.get(
+  '/user/:id',
+  requireAuth,
+  AuthController.checkAccess(1, 'timetable'),
+  //AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
+  trimRequest.all,
+  controller.getItemByUser
+)
 /*
  * Get all items route
  */
+
 router.get('/all', controller.getAllItems)
 
 /*
  * Get items route
  */
 router.get(
-    '/',
-    //requireAuth,
-    //AuthController.checkAccess(1, 'timetable'),
-    // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
-    trimRequest.all,
-    controller.getItems
+  '/',
+  //requireAuth,
+  //AuthController.checkAccess(1, 'timetable'),
+  // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
+  trimRequest.all,
+  controller.getItems
 )
 
 router.get(
-    '/last',
-    //requireAuth,
-    //AuthController.checkAccess(1, 'timetable'),
-    // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
-    trimRequest.all,
-    controller.getLastUpload
+  '/last',
+  //requireAuth,
+  //AuthController.checkAccess(1, 'timetable'),
+  // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
+  trimRequest.all,
+  controller.getLastUpload
 )
 
 /*
  * Create new item route
  */
 router.post(
-    '/',
-    //requireAuth,
-    //AuthController.checkAccess(4, 'timetable'),
-    // AuthController.roleAuthorization(['admin']),
-    trimRequest.all,
-    //validate.createItem,
-    controller.createItem
+  '/',
+  //requireAuth,
+  //AuthController.checkAccess(4, 'timetable'),
+  // AuthController.roleAuthorization(['admin']),
+  trimRequest.all,
+  //validate.createItem,
+  controller.createItem
 )
 
 /*
  * Create new item route
  */
 router.post(
-    '/dbf',
-    //requireAuth,
-    //AuthController.checkAccess(4, 'timetable'),
-    // AuthController.roleAuthorization(['admin']),
-    trimRequest.all,
-    //validate.createItem,
-    controller.createItemFromDBF
+  '/dbf',
+  //requireAuth,
+  //AuthController.checkAccess(4, 'timetable'),
+  // AuthController.roleAuthorization(['admin']),
+  trimRequest.all,
+  //validate.createItem,
+  controller.createItemFromDBF
 )
 
 /*
  * Get item route
  */
 router.get(
-    '/:id',
-    requireAuth,
-    AuthController.checkAccess(1, 'timetable'),
-    // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
-    trimRequest.all,
-    validate.getItem,
-    controller.getItem
+  '/:id',
+  requireAuth,
+  AuthController.checkAccess(1, 'timetable'),
+  // AuthController.roleAuthorization(['student', 'lecturer', 'admin']),
+  trimRequest.all,
+  validate.getItem,
+  controller.getItem
 )
 
 /*
  * Update item route
  */
 router.patch(
-    '/:id',
-    requireAuth,
-    AuthController.checkAccess(2, 'timetable'),
-    // AuthController.roleAuthorization(['admin']),
-    trimRequest.all,
-    validate.updateItem,
-    controller.updateItem
+  '/:id',
+  requireAuth,
+  AuthController.checkAccess(2, 'timetable'),
+  // AuthController.roleAuthorization(['admin']),
+  trimRequest.all,
+  validate.updateItem,
+  controller.updateItem
 )
 
 /*
  * Delete item route
  */
 router.delete(
-    '/:id',
-    requireAuth,
-    AuthController.checkAccess(4, 'timetable'),
-    // AuthController.roleAuthorization(['admin']),
-    trimRequest.all,
-    validate.deleteItem,
-    controller.deleteItem
+  '/:id',
+  requireAuth,
+  AuthController.checkAccess(4, 'timetable'),
+  // AuthController.roleAuthorization(['admin']),
+  trimRequest.all,
+  validate.deleteItem,
+  controller.deleteItem
 )
 
 module.exports = router
